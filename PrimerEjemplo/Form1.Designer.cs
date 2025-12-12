b7cf7678fddb13c56122bfebdbb7dd8b8416cc07
mmtol <espstok04@gmail.com>
2025-12-12 10:20:24 +0100

    Primer Comit

--- PrimerEjemplo/Form1.Designer.cs
+++ PrimerEjemplo/Form1.Designer.cs
@@ -28,18 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
+            btnUno = new Button();
+            txtNombre = new TextBox();
+            label1 = new Label();
             SuspendLayout();
+            // 
+            // btnUno
+            // 
+            btnUno.BackColor = SystemColors.Highlight;
+            btnUno.Location = new Point(260, 216);
+            btnUno.Name = "btnUno";
+            btnUno.Size = new Size(112, 37);
+            btnUno.TabIndex = 0;
+            btnUno.Text = "PÚLSAME";
+            btnUno.UseVisualStyleBackColor = false;
+            btnUno.Click += button1_Click_1;
+            // 
+            // txtNombre
+            // 
+            txtNombre.Location = new Point(164, 177);
+            txtNombre.Name = "txtNombre";
+            txtNombre.Size = new Size(324, 23);
+            txtNombre.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(164, 147);
+            label1.Name = "label1";
+            label1.Size = new Size(117, 15);
+            label1.TabIndex = 2;
+            label1.Text = "Introduce tu nombre";
             // 
             // Form1
             // 
+            AccessibleDescription = "";
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(395, 450);
+            BackColor = SystemColors.InactiveCaption;
+            ClientSize = new Size(644, 308);
+            Controls.Add(label1);
+            Controls.Add(txtNombre);
+            Controls.Add(btnUno);
+            HelpButton = true;
             Name = "Form1";
-            Text = "Form1";
+            Text = "FormFirst";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button btnUno;
+        private TextBox txtNombre;
+        private Label label1;
     }
 }
--- PrimerEjemplo/Form1.cs
+++ PrimerEjemplo/Form1.cs
@@ -6,5 +6,16 @@ namespace PrimerEjemplo
         {
             InitializeComponent();
         }
+
+        
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            this.txtNombre.Location = new Point(200,100);
+            this.txtNombre.Text = "soy un texto nuevo";
+            this.txtNombre.Width = 200;
+            this.txtNombre.TextAlign =  HorizontalAlignment.Center;
+            this.txtNombre.BackColor = Color.Aqua;
+        }
     }
 }
