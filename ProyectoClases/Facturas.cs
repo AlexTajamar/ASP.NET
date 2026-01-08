5937f1d04fb0f6033cec5f12912ff57a2a9e1f70
mmtol <espstok04@gmail.com>
2026-01-08 14:09:17 +0100

    Interfaces

---
+++ ProyectoClases/Facturas.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProyectoClases
+{
+    public class Facturas : IFacturas
+    {
+        public int PrecioBase { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int precioFinal { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int precioTotal { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public void CalcularIVA()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OdioHacienda(int mucho)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ProyectoClases/Interface1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ProyectoClases
+{
+    public interface IFacturas
+    {
+
+
+         int PrecioBase { get; set; }
+         int precioFinal { get; set; }
+        int precioTotal { get; set; }
+        void CalcularIVA();
+        void OdioHacienda(int mucho);
+    }
+}
